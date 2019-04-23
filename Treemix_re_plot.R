686cd5fe00eb707be090156dc1a2bebbf74825b9
dangliu <d901809d@gmail.com>
2019-04-23 20:17:31 +0200

    Treemix_re_plot.R modified to be mroe flexible

--- Treemix_re_plot.R
+++ Treemix_re_plot.R
@@ -14,18 +14,22 @@ source("/home/dang_liu/bin/BITE/R/treemix_fit.R")
 source("/home/dang_liu/bin/BITE/R/treemix_drift.R")
 source("/home/dang_liu/bin/BITE/R/newick_split.R")
 
+# Set up input files
+treemixout <- "/mnt/scratch/dang/Vietnam/TreeMix/outgroup.v2/boot2/HO.A.OG2.LLR.m4.re100"
+pop_list <- "/mnt/scratch/dang/Vietnam/TreeMix/outgroup.v2/pop.color.LLR.list"
 
+  
 # plot
-treemix.bootstrap("/mnt/scratch/dang/Vietnam/TreeMix/outgroup.v2/boot2/HO.A.OG2.LLR.m4.re100",
-                  out.file = "/mnt/scratch/dang/Vietnam/TreeMix/outgroup.v2/boot2/tmp",
-                  "/mnt/scratch/dang/Vietnam/TreeMix/outgroup.v2/boot2/HO.A.OG2.LLR.m4.re100_outtree.newick",
-                  pop.color.file="/mnt/scratch/dang/Vietnam/TreeMix/outgroup.v2/pop.color.LLR.list",
+treemix.bootstrap(treemixout,
+                  out.file = paste(treemixout, "tmp", sep="."),
+                  paste(treemixout, "_outtree.newick", sep="."),
+                  pop.color.file=pop_list,
                   100, disp=0.0005)
-plot_resid("/mnt/scratch/dang/Vietnam/TreeMix/outgroup.v2/boot2/HO.A.OG2.LLR.m4.re100",
-           "/mnt/scratch/dang/Vietnam/TreeMix/outgroup.v2/pop.color.LLR.list")
-treemix.drift("/mnt/scratch/dang/Vietnam/TreeMix/outgroup.v2/boot2/HO.A.OG2.LLR.m4.re100",
-              pop.order.color.file="/mnt/scratch/dang/Vietnam/TreeMix/outgroup.v2/pop.color.LLR.list")
+plot_resid(treemixout,
+           pop_list)
+treemix.drift(treemixout,
+              pop.order.color.file=pop_list)
 
-
+# Extract single tree here
 plot_tree("/mnt/scratch/dang/Vietnam/TreeMix/outgroup.v2/test.Try0",
           "/mnt/scratch/dang/Vietnam/TreeMix/outgroup.v2/pop.color.test.list")
