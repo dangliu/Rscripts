f30e8551cb2bc3ca974593c1cad7109e9a9fd747
dangliu <d901809d@gmail.com>
2019-04-24 17:59:42 +0200

    Treemix_re_plot.R modified

--- Treemix_re_plot.R
+++ Treemix_re_plot.R
@@ -15,15 +15,15 @@ source("/home/dang_liu/bin/BITE/R/treemix_drift.R")
 source("/home/dang_liu/bin/BITE/R/newick_split.R")
 
 # Set up input files
-treemixout <- "/mnt/scratch/dang/Vietnam/TreeMix/outgroup.v2/boot2/HO.A.OG2.LLR.m4.re100"
+treemixout <- "/mnt/scratch/dang/Vietnam/TreeMix/outgroup.v2/boot3/HO.A.OG2.LLR.m3.re100"
 pop_list <- "/mnt/scratch/dang/Vietnam/TreeMix/outgroup.v2/pop.color.LLR.list"
 
   
 # plot
 treemix.bootstrap(treemixout,
                   out.file = paste(treemixout, "tmp", sep="."),
-                  paste(treemixout, "_outtree.newick", sep="."),
-                  pop.color.file=pop_list,
+                  paste(treemixout, "_outtree.newick", sep=""),
+                  pop.color.file=pop_list, boot.legend.location.x=0.15, boot.legend.location.y=0.5,
                   100, disp=0.0005)
 plot_resid(treemixout,
            pop_list)
@@ -31,5 +31,5 @@ treemix.drift(treemixout,
               pop.order.color.file=pop_list)
 
 # Extract single tree here
-plot_tree("/mnt/scratch/dang/Vietnam/TreeMix/outgroup.v2/test.Try0",
-          "/mnt/scratch/dang/Vietnam/TreeMix/outgroup.v2/pop.color.test.list")
+plot_tree("/mnt/scratch/dang/Vietnam/TreeMix/outgroup.v2/boot3/bootstrap/HO.A.OG2.LLR.m3.re100_treemix_bootrep_34",
+          "/mnt/scratch/dang/Vietnam/TreeMix/outgroup.v2/pop.color.LLR.list")
