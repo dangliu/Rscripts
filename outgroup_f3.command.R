73ba0468ce9928315e53785ce0434fd426ec087b
dangliu <d901809d@gmail.com>
2022-06-21 11:57:18 +0200

    outgroup_f3.command.R modified

--- outgroup_f3.command.R
+++ outgroup_f3.command.R
@@ -23,11 +23,12 @@ prefix <- args[1]
 data <- eigenstrat(prefix)
 
 # pop
+# Exclude Mbuti
 ind <- read_ind(data)
 table(ind$label)
 pops <- unique(ind$label)
 pops
-pops <- pops[!pops %in% c("French","Mbuti")]
+pops <- pops[!pops %in% c("Mbuti")]
 
 
 # outgroup f3
@@ -46,6 +47,11 @@ f3_res = data %>% transversions_only() %>% f3(A=pops, B=pops, C="Mbuti")
 save(f3_res, file=paste(prefix, "Mbuti_f3.tv.Rdata", sep="."))
 write.table(f3_res, paste(prefix, "Mbuti_f3.tv.txt", sep="."), sep="\t", quote=F, row.names=F)
 
+
+# Now exclude French
+pops <- unique(ind$label)
+pops
+pops <- pops[!pops %in% c("French")]
 
 # outgroup f3
 msgn("Running f3 (outgroup is French)...")
@@ -69,4 +75,4 @@ msgn("All done!")
 
 
 
-# last_v20190621
+# last_v20200720
